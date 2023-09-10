8aa92e09bec1c668d2d90b2ec6e5ac642b95e3cd
RahulMehra-1993 <1993rahulmehra1993@gmail.com>
2023-09-10 14:29:13 +0530

    updated version4

--- script.js
+++ script.js
@@ -11,10 +11,10 @@ const active = document.querySelector(".active");
 let placeName, data, dataArray, tempArray, key;
 
 searchButton.addEventListener("click", function () {
+  input.classList.remove("empty");
   //ask for a key
-  key = prompt("Limited Access key");
-  //loader on
-  input.classList.add("flip");
+  key = "b318998db8mshc05ceb3cdc0acbap1118c9jsnc2acfdfdf1b3";
+
   // console.log(input.value)
   placeName = input.value;
   //image clearing out
@@ -36,72 +36,79 @@ searchButton.addEventListener("click", function () {
     };
 
     try {
-      const response = await fetch(url, options);
-      const data = await response.json();
-      let { locations } = data;
-
-      for (let x of Object.values(locations)) {
-        for (let y of Object.values(x)) {
-          dataArray.push(y);
-        }
-      }
-      console.log(dataArray);
-      //finding country
-      let stringAddress = dataArray[3].split(",");
-      let stringCountry = stringAddress[stringAddress.length - 1];
-      console.log(stringCountry);
-      let flagCode = stringCountry.slice(1, 3);
-      console.log(flagCode);
-      //weather of the capital
-
-      dataArray[1].forEach((x) => {
-        for (let y of Object.entries(x)) {
-          tempArray.push(y);
-        }
-      });
-
-      console.log(tempArray);
-
-      let icon, icon2;
-      if (
-        tempArray[24][1].trim().toLowerCase().includes("overcast") ||
-        tempArray[24][1].trim().toLowerCase().includes("partiallycloudy")
-      ) {
-        icon = "🌦️";
-        active.classList.add("cloudy");
-      } else if (
-        tempArray[24][1].trim().toLowerCase().includes("rainy") ||
-        tempArray[24][1].trim().toLowerCase().includes("rain") ||
-        tempArray[24][1].trim().toLowerCase().includes("rain,overcast")
-      ) {
-        icon = "⛈️";
-        active.classList.add("rainy");
+      if (placeName === "") {
+        input.classList.add("empty");
+        return;
       } else {
-        icon = "☀️";
+        //loader on
+        input.classList.add("flip");
+        //fetch
+        const response = await fetch(url, options);
+        const data = await response.json();
+        let { locations } = data;
 
-        active.classList.add("sunny");
-      }
-      if (
-        tempArray[51][1].trim().toLowerCase().includes("overcast") ||
-        tempArray[51][1].trim().toLowerCase().includes("partiallycloudy")
-      ) {
-        icon2 = "🌦️";
-        active.classList.add("cloudy");
-      } else if (
-        tempArray[51][1].trim().toLowerCase().includes("rainy") ||
-        tempArray[51][1].trim().toLowerCase().includes("rain") ||
-        tempArray[51][1].trim().toLowerCase().includes("rain,overcast")
-      ) {
-        icon2 = "⛈️";
-        active.classList.add("rainy");
-      } else {
-        icon2 = "☀️";
+        for (let x of Object.values(locations)) {
+          for (let y of Object.values(x)) {
+            dataArray.push(y);
+          }
+        }
+        console.log(dataArray);
+        //finding country
+        let stringAddress = dataArray[3].split(",");
+        let stringCountry = stringAddress[stringAddress.length - 1];
+        console.log(stringCountry);
+        let flagCode = stringCountry.slice(1, 3);
+        console.log(flagCode);
+        //weather of the capital
 
-        active.classList.add("sunny");
-      }
+        dataArray[1].forEach((x) => {
+          for (let y of Object.entries(x)) {
+            tempArray.push(y);
+          }
+        });
 
-      //destructuring
-      firstBox.innerHTML = `
+        console.log(tempArray);
+
+        let icon, icon2;
+        if (
+          tempArray[24][1].trim().toLowerCase().includes("overcast") ||
+          tempArray[24][1].trim().toLowerCase().includes("partiallycloudy")
+        ) {
+          icon = "🌦️";
+          active.classList.add("cloudy");
+        } else if (
+          tempArray[24][1].trim().toLowerCase().includes("rainy") ||
+          tempArray[24][1].trim().toLowerCase().includes("rain") ||
+          tempArray[24][1].trim().toLowerCase().includes("rain,overcast")
+        ) {
+          icon = "⛈️";
+          active.classList.add("rainy");
+        } else {
+          icon = "☀️";
+
+          active.classList.add("sunny");
+        }
+        if (
+          tempArray[51][1].trim().toLowerCase().includes("overcast") ||
+          tempArray[51][1].trim().toLowerCase().includes("partiallycloudy")
+        ) {
+          icon2 = "🌦️";
+          active.classList.add("cloudy");
+        } else if (
+          tempArray[51][1].trim().toLowerCase().includes("rainy") ||
+          tempArray[51][1].trim().toLowerCase().includes("rain") ||
+          tempArray[51][1].trim().toLowerCase().includes("rain,overcast")
+        ) {
+          icon2 = "⛈️";
+          active.classList.add("rainy");
+        } else {
+          icon2 = "☀️";
+
+          active.classList.add("sunny");
+        }
+
+        //destructuring
+        firstBox.innerHTML = `
          <p class="capital">${dataArray[3]}</br></br>
          <img src="https://flagcdn.com/48x36/${flagCode.toLowerCase()}.png" alt="flag">
             </p>
@@ -120,14 +127,15 @@ searchButton.addEventListener("click", function () {
 
          </p>
       `;
-      secondBox.innerHTML = `
+        secondBox.innerHTML = `
        <p class="weather">Tomorrow's Tempertaure </br></br><span>${tempArray[40][1]}°c</span>
         <p class="Description">Description <ion-icon name="book-outline"></ion-icon></br></br>
          <span>${tempArray[51][1]} ${icon2}</span>
            </p>
       `;
-      //loader off
-      input.classList.remove("flip");
+        //loader off
+        input.classList.remove("flip");
+      }
     } catch (error) {
       console.error(error);
     }
